2113091594235f2d12576ef72743a8b85dd8a1a1
YousefKullab <the1yousef98@gmail.com>
2023-08-09 18:30:46 +0300

    update config files and install some loaders and plugins

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,28 @@
-const path = require("path");
-const webpack = require("webpack");
-
+const path = require('path')
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
     module: {
         rules: [
             {
-                test: /\.js$/, 
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
-                test: /.scss$/,
+                test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
@@ -24,6 +31,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
-};
+}
