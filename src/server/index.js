44d5e9b2f1998456b68d440f3fb603940a06b611
YousefKullab <the1yousef98@gmail.com>
2023-08-22 18:41:55 +0300

    create a post method on server side to post data i fetch it for api

--- src/server/index.js
+++ src/server/index.js
@@ -9,24 +9,13 @@ const MeaningCloud = require('meaning-cloud');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-var textapi = new MeaningCloud ({
-    application_key: process.env.API_KEY
-});
-
-var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
-
 const app = express()
 app.use(cors())
 // to use json
 app.use(bodyParser.json())
 // to use url encoded values
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }))
 
 app.use(express.static('dist'))
@@ -35,13 +24,31 @@ console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-})
+});
 
 app.get('/test', function (req, res) {
     res.json(mockAPIResponse);
-})
+});
+
+
+app.post('/evaluate', async (req, res) =>{
+    const article = req.body;
+    api_key: process.env.API_KEY;
+    const api_url = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&lang=en&txt=${encodeURIComponent(article)}`;
+
+    try{
+        const response = await fetch(api_url);
+        const data = await response.json();
+        console.log(data);
+        
+        res.json(data); // Send the response back to the client
+    }catch(err){
+        console.error('NLP Error:', err);
+    }
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
+
